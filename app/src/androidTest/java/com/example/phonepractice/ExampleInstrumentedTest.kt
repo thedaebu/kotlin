876462d3a7ce6331d4dfdbaf2475b405f47b9440
thedaebu <min.kim163@gmail.com>
2023-12-30 18:43:26 -0500

    12/30/23

--- app/src/androidTest/java/com/example/phonepractice/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/phonepractice/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.practice
+package com.example.phonepractice
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.practice", appContext.packageName)
+        assertEquals("com.example.phonepractice", appContext.packageName)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Practice"
+rootProject.name = "PhonePractice"
 include(":app")
+ 
