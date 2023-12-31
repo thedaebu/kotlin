a5245dfdde95e102256c8b72606b2fc3cc2969f4
thedaebu <min.kim163@gmail.com>
2023-12-31 16:09:59 -0500

    12/31/23

--- app/src/main/java/com/example/phonepractice/MainActivity.kt
+++ app/src/main/java/com/example/phonepractice/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.phonepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,6 +21,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,7 +42,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    GreetingText("Android")
+                    TutorialBox()
                 }
             }
         }
@@ -40,31 +50,169 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan, modifier = Modifier.fillMaxSize()) {
-        Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(8.dp)) {
-            Text(
-                text = message,
-                modifier = modifier.padding(24.dp),
-                fontSize = 30.sp,
-                lineHeight = 116.sp,
-                textAlign = TextAlign.Center
-            )
-            Text(
-                text = "From, $from",
-                fontSize = 36.sp,
-                modifier = Modifier
-                    .padding(16.dp)
-                    .align(alignment = Alignment.End)
-            )
+fun TutorialBox(modifier: Modifier = Modifier) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        TutorialContent()
+    }
+}
+
+@Composable
+fun TutorialContent(modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(R.drawable.article_header_image),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.article_title),
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.article_p1),
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .padding(end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(R.string.article_p2),
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Composable
+fun TaskManagerBox(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        TaskManagerContent()
+    }
+}
+
+@Composable
+fun TaskManagerContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.task_completed_icon),
+            contentDescription = null
+        )
+        Text(
+            text = "All Tasks Completed!",
+            modifier = modifier
+                .padding(top = 24.dp)
+                .padding(bottom = 8.dp)
+        )
+        Text(
+            text = "Nice Work!",
+            modifier = modifier,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Composable
+fun QuadrantContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Row(modifier = modifier.weight(1f)) {
+            QuadrantPart(stringResource(R.string.quadrant_title1), stringResource(R.string.quadrant_content1), Color(0xFFEADDFF), Modifier.weight(1F))
+            QuadrantPart(stringResource(R.string.quadrant_title2), stringResource(R.string.quadrant_content2), Color(0xFFD0BCFF), Modifier.weight(1F))
         }
+        Row(modifier = modifier.weight(1f)) {
+            QuadrantPart(stringResource(R.string.quadrant_title3), stringResource(R.string.quadrant_content3), Color(0xFFB69DF8), Modifier.weight(1F))
+            QuadrantPart(stringResource(R.string.quadrant_title4), stringResource(R.string.quadrant_content4), Color(0xFFF6EDFF), Modifier.weight(1F))
+        }
+    }
+}
+
+@Composable
+fun QuadrantPart(title: String, content: String, color: Color, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier
+                .padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = content,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Composable
+fun BusinessCardContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFFB69DF8))
+    ) {
+        BusinessCardTop(modifier = Modifier.weight(1f))
+        BusinessCardBottom(modifier = Modifier.weight(1f))
+    }
+}
+
+@Composable
+fun BusinessCardTop(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.business_card_image),
+            contentDescription = null,
+            contentScale = ContentScale.Inside,
+            modifier = Modifier.background(Color(0xFFF6EDFF)).size(50.dp)
+        )
+        Text(
+            text = "My Name"
+        )
+        Text(
+            text = "Software Engineer"
+        )
+    }
+}
+
+@Composable
+fun BusinessCardBottom(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(bottom = 15.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "phone number"
+        )
+        Text(
+            text = "email"
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Preview() {
     PhonePracticeTheme {
-        GreetingText(message = "Happy birthday!", from = "Me")
+        BusinessCardContent()
     }
 }
