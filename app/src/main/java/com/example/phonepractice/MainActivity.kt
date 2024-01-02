723a62dc8780498025408384bb50cbb6ed789c78
thedaebu <min.kim163@gmail.com>
2024-01-02 12:54:37 -0500

    add tipcalculator

--- app/src/main/java/com/example/phonepractice/MainActivity.kt
+++ app/src/main/java/com/example/phonepractice/MainActivity.kt
@@ -12,12 +12,22 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,92 +40,24 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.phonepractice.ui.theme.PhonePracticeTheme
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PhonePracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    TutorialBox()
-                }
+                CalculatorShow()
             }
         }
     }
-}
 
-@Composable
-fun TutorialBox(modifier: Modifier = Modifier) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        TutorialContent()
-    }
-}
-
-@Composable
-fun TutorialContent(modifier: Modifier = Modifier) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(
-            painter = painterResource(R.drawable.article_header_image),
-            contentDescription = null
-        )
-        Text(
-            text = stringResource(R.string.article_title),
-            fontSize = 24.sp,
-            modifier = Modifier
-                .padding(16.dp)
-        )
-        Text(
-            text = stringResource(R.string.article_p1),
-            modifier = Modifier
-                .padding(start = 16.dp)
-                .padding(end = 16.dp),
-            textAlign = TextAlign.Justify
-        )
-        Text(
-            text = stringResource(R.string.article_p2),
-            modifier = Modifier
-                .padding(16.dp),
-            textAlign = TextAlign.Justify
-        )
-    }
-}
-
-@Composable
-fun TaskManagerBox(modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxSize()) {
-        TaskManagerContent()
-    }
-}
-
-@Composable
-fun TaskManagerContent(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .fillMaxHeight(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(R.drawable.task_completed_icon),
-            contentDescription = null
-        )
-        Text(
-            text = "All Tasks Completed!",
-            modifier = modifier
-                .padding(top = 24.dp)
-                .padding(bottom = 8.dp)
-        )
-        Text(
-            text = "Nice Work!",
-            modifier = modifier,
-            fontSize = 16.sp
-        )
+    override fun onStart() {
+        super.onStart()
     }
 }
 
@@ -180,7 +122,9 @@ fun BusinessCardTop(modifier: Modifier = Modifier) {
             painter = painterResource(R.drawable.business_card_image),
             contentDescription = null,
             contentScale = ContentScale.Inside,
-            modifier = Modifier.background(Color(0xFFF6EDFF)).size(50.dp)
+            modifier = Modifier
+                .background(Color(0xFFF6EDFF))
+                .size(50.dp)
         )
         Text(
             text = "My Name"
@@ -209,10 +153,127 @@ fun BusinessCardBottom(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun LemonSqueeze(modifier: Modifier = Modifier) {
+    val stages: List<String> = listOf("lemon_tree", "lemon_squeeze", "lemon_drink", "lemon_restart")
+    var stageIndex by remember { mutableIntStateOf(1) }
+    val imageResource = when(stageIndex) {
+        0 -> R.drawable.lemon_tree
+        1 -> R.drawable.lemon_squeeze
+        2 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LemonSqueezeTitle()
+        LemonSqueezeContent(imageResource,
+            stages[stageIndex],
+            onImageClick = { stageIndex = (stageIndex + 1) % 4 }
+        )
+    }
+}
+
+@Composable
+fun LemonSqueezeTitle(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.Yellow)
+            .height(30.dp)
+    ) {
+        Text(
+            text = "Lemonade",
+            fontSize = 20.sp,
+            modifier = modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun LemonSqueezeContent(imageResource: Int, content: String, onImageClick: () -> Unit, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = onImageClick,
+                shape = RoundedCornerShape(40.dp)
+            ) {
+                Image(
+                    painter = painterResource(imageResource),
+                    contentDescription = null
+                )
+            }
+            Text(
+                text = content
+            )
+        }
+    }
+}
+
+@Composable
+fun CalculatorShow(modifier: Modifier = Modifier) {
+    var costInput by remember { mutableStateOf("") }
+    var cost = costInput.toDoubleOrNull() ?: 0.0
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Calculate Tip",
+            modifier = modifier
+                .padding(start = 70.dp)
+                .align(alignment = Alignment.Start)
+
+        )
+        CalculatorField(costInput, onCostFieldChange = { costInput = it })
+        TipShow(cost)
+    }
+}
+
+@Composable
+fun CalculatorField(cost: String, onCostFieldChange: (String) -> Unit, modifier: Modifier = Modifier) {
+    TextField(
+        label = { Text( text = "Bill Amount" ) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        value = cost,
+        onValueChange = onCostFieldChange,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun TipShow(cost: Double, modifier: Modifier = Modifier) {
+    val tipAmount = (cost * .15).toString()
+    Box(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "Tip Amount: $tipAmount",
+            modifier = modifier
+                .align(alignment = Alignment.Center)
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun Preview() {
     PhonePracticeTheme {
-        BusinessCardContent()
+        CalculatorShow()
     }
 }
