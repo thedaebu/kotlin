be425c32b1cdd634154ea61fac20c8865ae2b748
thedaebu <min.kim163@gmail.com>
2024-01-04 22:46:45 -0500

    move screens to new screens module

---
+++ app/src/main/java/com/example/phonepractice/ui/screens/GameViewModel.kt
@@ -0,0 +1,88 @@
+package com.example.phonepractice.ui.screens
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import com.example.phonepractice.data.allWords
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.flow.update
+import com.example.phonepractice.data.SCORE_INCREASE
+import com.example.phonepractice.data.MAX_NO_OF_WORDS
+
+class GameViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+    var userGuess by mutableStateOf("")
+        private set
+
+    init {
+        resetGame()
+    }
+
+    private fun pickRandomAndShuffle(): String {
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord)) return pickRandomAndShuffle()
+
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord) == (word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomAndShuffle())
+    }
+
+    fun updateUserGuess(guess: String) {
+        userGuess = guess
+    }
+
+    fun checkUserGuess() {
+        if (userGuess.equals(currentWord, ignoreCase = true)) {
+            val updatedScore = _uiState.value.score.plus(SCORE_INCREASE)
+            updateGameState(updatedScore)
+        } else {
+            _uiState.update { currentState -> currentState.copy(isGuessedWordWrong = true) }
+        }
+        updateUserGuess("")
+    }
+
+    private fun updateGameState(updatedScore: Int) {
+        if (usedWords.size == MAX_NO_OF_WORDS) {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    score = updatedScore,
+                    isGameOver = true
+                )
+            }
+        } else {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    currentScrambledWord = pickRandomAndShuffle(),
+                    score = updatedScore,
+                    currentWordCount = currentState.currentWordCount.inc()
+                )
+            }
+        }
+    }
+
+    fun skipWord() {
+        updateGameState(_uiState.value.score)
+        updateUserGuess("")
+    }
+}
---
+++ app/src/main/java/com/example/phonepractice/ui/screens/LemonSqueeze.kt
@@ -0,0 +1,93 @@
+package com.example.phonepractice.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.phonepractice.R
+
+@Composable
+fun LemonSqueeze(modifier: Modifier = Modifier) {
+    val stages: List<String> = listOf("lemon_tree", "lemon_squeeze", "lemon_drink", "lemon_restart")
+    var stageIndex by remember { mutableIntStateOf(1) }
+    val imageResource = when(stageIndex) {
+        0 -> R.drawable.lemon_tree
+        1 -> R.drawable.lemon_squeeze
+        2 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LemonSqueezeTitle()
+        LemonSqueezeContent(imageResource,
+            stages[stageIndex],
+            onImageClick = { stageIndex = (stageIndex + 1) % 4 }
+        )
+    }
+}
+
+@Composable
+fun LemonSqueezeTitle(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.Yellow)
+            .height(30.dp)
+    ) {
+        Text(
+            text = "Lemonade",
+            fontSize = 20.sp,
+            modifier = modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun LemonSqueezeContent(imageResource: Int, content: String, onImageClick: () -> Unit, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = onImageClick,
+                shape = RoundedCornerShape(40.dp)
+            ) {
+                Image(
+                    painter = painterResource(imageResource),
+                    contentDescription = null
+                )
+            }
+            Text(
+                text = content
+            )
+        }
+    }
+}
